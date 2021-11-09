5f6539481dfb1cdae0b2707e00d40025e1002bbd
Pe-Guedss <pedro.og2002@gmail.com>
2021-11-09 16:20:44 -0300

    Arquivo principal de chamada das funções

---
+++ testes/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "media.h"
+
+using std::cout;
+using std::endl;
+
+int main (){
+    int v[] = {13, 17, 42, 56, 7};
+    cout << "A media dos valores do vetor é " << media (5, v) << endl;
+    return 0;
+}
